78973b479ee5846f325279e3d2037aa27a5607cc
Richard Yu <vipzhicheng@gmail.com>
2021-07-27 11:12:31 +0800

    fix: adjust README.md

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Logseq Markmap Plugin
 
+![Screenshot 1](screenshot%201.png)
+
+![Screenshot 2](screenshot%202.png)
+
+This is a plugin for [Logseq](https://github.com/logseq/logseq) to provide mindmap support based on [Markmap](https://github.com/gera2ld/markmap).
+
 ## Installation and Usage
 
 ```
 npm install
-npm run build
+npm run build:prod # For real
+npm run build # For development
 ```
 
 load unpack plugin from `dist` directory
@@ -13,8 +20,35 @@ load unpack plugin from `dist` directory
 
 * Typescript
 * Tailwind
-* Markmap
+* Markmap related packages
 
+## Features
+
+Most of features come from `Markmap` project.
+
+* Colorful mindmap items.
+* Markmap toolbar included.
+* Support inline code and code block.
+* Support inline text styles.
+* Support normal links.
+* Support inline latex syntax.
+* Support mouse drag and drop, double click, scrollwheel zoom.
+
+## Notes
+
+* The mindmap is not editable.
+* Properies are ignored.
+* `#` Tags are ignored.
+* Text length is unlimited.
+* `#` head markdown syntax is ignored, use hierarchical blocks instead.
+* image not supported.
+* Hiccup not supported.
+* Logseq embeded feature not supported.
+* Only tested on `Markdown` mode, not `Org` mode, sorry!
+* Tested on Logseq `v0.2.8` with the temp plugin infrastructure.
+## Contribution
+
+Issues and PRs are welcome!
 
 ## Licence
 
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import '@logseq/libs';
 import { Transformer } from 'markmap-lib';
 import * as markmap from 'markmap-view';
 import { Markmap } from 'markmap-view';
+import { Toolbar } from 'markmap-toolbar';
 
 const transformer = new Transformer();
 
@@ -28,7 +29,7 @@ async function main() {
     key: 'logseq-mark-map',
     template: `
      <a data-on-click="openMindMap" title="open mind map 2">
-       x
+      <svg t="1627350023942" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="670" width="18" height="18"><path d="M840.533333 490.666667l-17.066666-85.333334L554.666667 460.8V170.666667h-85.333334v262.4l-296.533333-192-46.933333 72.533333 324.266666 209.066667L200.533333 849.066667l68.266667 51.2 241.066667-315.733334 179.2 270.933334 72.533333-46.933334-179.2-266.666666z" fill="#CFD8DC" p-id="671"></path><path d="M512 512m-149.333333 0a149.333333 149.333333 0 1 0 298.666666 0 149.333333 149.333333 0 1 0-298.666666 0Z" fill="#C435F3" p-id="672" data-spm-anchor-id="a313x.7781069.0.i0" class=""></path><path d="M512 170.666667m-106.666667 0a106.666667 106.666667 0 1 0 213.333334 0 106.666667 106.666667 0 1 0-213.333334 0Z" fill="#F48233" p-id="673" data-spm-anchor-id="a313x.7781069.0.i4" class="selected"></path><path d="M832 448m-106.666667 0a106.666667 106.666667 0 1 0 213.333334 0 106.666667 106.666667 0 1 0-213.333334 0Z" fill="#F48233" p-id="674" data-spm-anchor-id="a313x.7781069.0.i5" class="selected"></path><path d="M149.333333 277.333333m-106.666666 0a106.666667 106.666667 0 1 0 213.333333 0 106.666667 106.666667 0 1 0-213.333333 0Z" fill="#F48233" p-id="675" data-spm-anchor-id="a313x.7781069.0.i3" class="selected"></path><path d="M234.666667 874.666667m-106.666667 0a106.666667 106.666667 0 1 0 213.333333 0 106.666667 106.666667 0 1 0-213.333333 0Z" fill="#F48233" p-id="676" data-spm-anchor-id="a313x.7781069.0.i7" class="selected"></path><path d="M725.333333 832m-106.666666 0a106.666667 106.666667 0 1 0 213.333333 0 106.666667 106.666667 0 1 0-213.333333 0Z" fill="#F48233" p-id="677" data-spm-anchor-id="a313x.7781069.0.i6" class="selected"></path></svg>
      </a>
     `,
   });
@@ -69,6 +70,7 @@ async function main() {
   };
 
   let mm: Markmap;
+
   logseq.on('ui:visible:changed', async ({ visible }) => {
     if (!visible) {
       return;
@@ -101,6 +103,17 @@ async function main() {
         },
         root
       );
+
+      // Customize toolbar
+      const toolbar = new Toolbar();
+      toolbar.setItems(['zoomIn', 'zoomOut', 'fit']);
+      toolbar.setBrand(false);
+      toolbar.attach(mm);
+      const el = toolbar.render();
+      el.style.position = 'absolute';
+      el.style.bottom = '0.5rem';
+      el.style.right = '0.5rem';
+      document.getElementById('markmap-container')?.append(el);
     }
   });
 }
