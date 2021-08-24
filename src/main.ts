b912d58a2599794a08266cd1fb3085bc62b18f0d
Richard Yu <vipzhicheng@gmail.com>
2021-08-24 19:20:23 +0800

    fix: save as png file name

--- src/main.ts
+++ src/main.ts
@@ -455,6 +455,7 @@ async function main() {
           '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>',
         onClick: async () => {
           let el = document.getElementById('markmap-container');
+          const page = await logseq.Editor.getCurrentPage();
           if (el) {
             html2canvas(el).then(function (canvas) {
               const title = page?.originalName;
